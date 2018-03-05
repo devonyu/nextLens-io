53a321182ce2da290f48b418b81c2c7d1a4ae5e2
Devon Yu <devonyu415@gmail.com>
2018-03-04 22:59:03 -0800

    Found bug, node version not up to date for async call

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -3,15 +3,10 @@ import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import './App.css';
-import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-// render((
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// ), document.getElementById('root'));
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
