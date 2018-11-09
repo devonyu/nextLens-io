9984a4594e730f69862fbfa491eeeb9b6d4207f9
Devon Yu <devonyu415@gmail.com>
2018-11-09 08:56:39 -0800

    Swiping landing page with auto swipe implemented

--- react-ui/src/components/Landing.jsx
+++ react-ui/src/components/Landing.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Container } from 'semantic-ui-react';
-//import SwipingImages from './SwipingImages';
-import ClickLanding from './ClickLanding';
+import SwipeLanding from './SwipeLanding';
 
 export default class Landing extends Component {
     constructor(props) {
@@ -14,7 +13,7 @@ export default class Landing extends Component {
     }
 
     componentDidMount () {
-        console.log('Landing Page mounted!')
+        console.log('Landing Page mounted!');
     }
 
     render() {
@@ -24,10 +23,7 @@ export default class Landing extends Component {
                 <h2>Welcome to Nextlens.io</h2>
                 <Button basic color='green' size='large' content='Sign up for free' onClick={this.toggleSignup}/>
                 </Container>
-                <ClickLanding>
-                </ClickLanding>
-                {/* <SwipingImages>
-                </SwipingImages> */}
+                <SwipeLanding/>
             </Container>
         )
     }
---
+++ react-ui/src/components/SwipeLanding.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Container, Image } from 'semantic-ui-react';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+export default class SwipeLanding extends Component {
+    constructor(props) {
+        super(props);
+		this.state = {
+			imgs: [],
+            currentIndex: 0,
+        }
+        this.getSplashImages = this.getSplashImages.bind(this);
+    }
+
+    getSplashImages = () => {
+		axios.get('/landing')
+		.then(({ data }) => {
+            const temp = [];
+            data.results.forEach((img)=> {
+                temp.push(img.urls.regular);
+            })
+            this.setState(() => {
+                return {
+                    imgs: temp,
+                    currentIndex: 0
+                };
+            });
+		})
+		.catch((error) => {
+		    console.log(error);
+		});
+    }
+
+    componentDidMount () {
+        this.getSplashImages();
+    }
+
+    render() {
+        return(
+                <Container id="splashWrap">
+                    <AutoPlaySwipeableViews 
+                    enableMouseEvents
+                    animateTransitions
+                    resistance
+                    autoplay
+                    >
+                        {this.state.imgs.map((image) => {
+                            return <div>
+                                <Image id="splashImage" src={image}/>
+                            </div>
+                        })}
+                    </AutoPlaySwipeableViews>
+                </Container>
+        )
+    }
+}
