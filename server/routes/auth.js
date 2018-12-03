a42eb5ad14527ea56d282035b5fcf70bf65b2c36
Devon Yu <devonyu415@gmail.com>
2018-12-03 01:11:33 -0800

    env for developement and production and code cleanup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../../database/db');
+const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('../../database/db');
+
+const router = express.Router();
 router.use(bodyParser.json());
 
-router.use('/',  async (req, res, next) => {
-    // create new session object.
-    if(req.session && req.session.key) {
-    // if email key is sent redirect.
-      console.log('Users Session within Server=> ', req.session)
-      // get user information from DB
-      let user = req.session.key;
-      console.log('user found: ', user);
-      let results = await db.checkUserId(user);
-      delete results.password;
-      console.log('results of auth: ', results);
-      res.send(results);
-    } else {
-    // else go to home page.
-      console.log('key not present, No user session located');
-      //return next();
-      res.send({'error': 'NOT AUTHENTICATED'});
-    }
-  });
+router.use('/', async (req, res) => {
+  // create new session object.
+  if (req.session && req.session.key) {
+  // if email key is sent redirect.
+    console.log('Users Session within Server=> ', req.session);
+    // get user information from DB
+    const user = req.session.key;
+    console.log('user found: ', user);
+    const results = await db.checkUserId(user);
+    delete results.password;
+    console.log('results of auth: ', results);
+    res.send(results);
+  } else {
+  // else go to home page.
+    console.log('key not present, No user session located');
+    // return next();
+    res.send({ error: 'NOT AUTHENTICATED' });
+  }
+});
 
-  module.exports = router;
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,47 +1,46 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
-const db = require('../database/db');
-//const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const uuidv4 = require('uuid/v4')
+const uuidv4 = require('uuid/v4');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const app = express();
 const session = require('express-session');
 const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
+
+const app = express();
 dotenv.load();
 const client = redis.createClient(process.env.REDIS_URL);
-const RedisStore = require('connect-redis')(session);
+
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
-//app.use(cors);
+// app.use(cors);
 app.use(cookieParser());
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-client.on('connect', function() {
+client.on('connect', () => {
   console.log('redis connected');
 });
 
 app.use(session({
-  //store: new RedisStore({host: 'localhost', port: 6379, client: client, ttl: 60}),
-  store: new RedisStore({client: client, ttl: 60000}),
+  // store: new RedisStore({host: 'localhost', port: 6379, client: client, ttl: 60}),
+  store: new RedisStore({ client, ttl: 60000 }),
   id: uuidv4,
   secret: process.env.REDIS_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: {}
+  cookie: {},
 }));
 
-var router = require('./routes/router')
-app.use('/', router);
+const router = require('./routes/router');
 
+app.use('/', router);
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', (request, response) => {
-  console.log('catch all with *')
+  console.log('catch all with *');
   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
