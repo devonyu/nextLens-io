4591b4efd0e9342f56b44dd94d7d37de28c49fb8
Devon Yu <devonyu415@gmail.com>
2020-01-12 02:40:52 -0800

    functional modal controled

--- react-ui/src/components/ModalControlled.jsx
+++ react-ui/src/components/ModalControlled.jsx
@@ -1,48 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Icon, Modal } from 'semantic-ui-react';
 
-export default class ModalControlled extends Component {
-  state = { modalOpen: false };
+const ModalControlled = inputProps => {
+  const { message, open, close } = inputProps;
+  return (
+    <Modal
+      open={open}
+      onClose={() => close(false)}
+      basic
+      size="small"
+      closeOnEscape
+      closeOnDimmerClick
+    >
+      <Modal.Content>
+        <h3>{message}</h3>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="green" onClick={() => close(false)} inverted>
+          <Icon name="checkmark" /> Got it
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
 
-  componentWillReceiveProps(props) {
-    if (props.open) {
-      this.handleOpen();
-    }
-  }
-
-  handleOpen = () => this.setState({ modalOpen: true });
-
-  handleClose = () => {
-    const { close } = this.props;
-    this.setState({ modalOpen: false });
-    close(false, '');
-  };
-
-  render() {
-    const { message } = this.props;
-    const { modalOpen } = this.state;
-    return (
-      <Modal
-        open={modalOpen}
-        onClose={this.handleClose}
-        basic
-        size="small"
-        closeOnEscape
-        closeOnDimmerClick
-      >
-        <Modal.Content>
-          <h3>{message}</h3>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button color="green" onClick={this.handleClose} inverted>
-            <Icon name="checkmark" /> Got it
-                              </Button>
-        </Modal.Actions>
-      </Modal>
-    );
-  }
-}
+export default ModalControlled;
 
 ModalControlled.propTypes = {
   open: PropTypes.bool.isRequired,
