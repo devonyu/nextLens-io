531ee9b2cad483b2e85f0c6025f8f28cd5a9896d
Devon Yu <devonyu415@gmail.com>
2020-01-11 23:31:15 -0800

    update travis node version and jest snapshots

--- react-ui/src/__test__/components/Profile.test.js
+++ react-ui/src/__test__/components/Profile.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Profile from '../../components/Profile';
 import renderer from 'react-test-renderer';
- 
+
 test('Loads Profile Card with Props', () => {
-    const userInformation = {
-        'profileimgurl': 'http://test.com/image.jpg',
-        'firstname' : 'Tester',
-        'mount': '3',
-    }
-    const component = renderer.create(
-        <Profile userInformation={userInformation} />
-    );
-    let tree = component.toJSON();
-expect(tree).toMatchSnapshot();
+  const userInformation = {
+    firstname: 'Tester',
+    mount: '3',
+    email: 'devonyu415@gmail.com'
+  };
+  const component = renderer.create(<Profile userInformation={userInformation} />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
